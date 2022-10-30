848a3dcf3f4397789f2c2f67e138f5607c44058c
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-10-30 16:44:16 +0300

    realised the work DrugInfoViewController

---
+++ PillBoxApp/ViewControllers/DrugInfoViewController.swift
@@ -0,0 +1,26 @@
+//
+//  DrugInfoViewController.swift
+//  PillBoxApp
+//
+//  Created by jopootrivatel on 30.10.2022.
+//
+
+import UIKit
+
+class DrugInfoViewController: UIViewController {
+
+    
+    @IBOutlet var photoImageView: UIImageView!
+    @IBOutlet var descriptionLabel: UILabel!
+    
+    var drug: Drug!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        navigationItem.title = drug.drugName
+        photoImageView.image = UIImage(named: drug.drugName)
+        descriptionLabel.text = drug.description
+    }
+
+}
