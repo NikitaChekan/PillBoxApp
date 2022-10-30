bae4f689b2892a42f37feece9967fb7ecd4b769b
NikitaChekan <110492748+NikitaChekan@users.noreply.github.com>
2022-10-30 15:31:19 +0300

    Initial Commit

---
+++ PillBoxApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PillBoxApp
+//
+//  Created by jopootrivatel on 30.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
